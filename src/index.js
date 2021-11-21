677d069dcc94a7471c9e03692911d9423954b1cd
David Seco Diaz <dsecodi@corp.vodafone.es>
2021-11-21 16:03:07 +0100

    Add serviceWorkerRegistration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 //Store
 import store from './redux/index';
 
@@ -15,7 +17,7 @@ ReactDOM.render(
   
   document.getElementById('root')
 );
-
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
